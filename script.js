38cf3485adecda5c3c92c0439fa25da762c925c6
Anandh Kumar <accetanandhmca@gmail.com>
2023-08-01 22:07:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,78 @@
+// Initialize cart count
+let cartCount = 0;
+
+// Create the container for product display
 const container = document.createElement("div");
 container.setAttribute("class", "container");
 
-const heading = document.createElement("h1");
-heading.setAttribute("id","title");
-heading.setAttribute("class","text-center");
-heading.innerHTML = "Displaying Store Data using FakeStoreAPI";
-document.body.appendChild(heading);
-
+// Create a row within the container
 const row = document.createElement("div");
 row.setAttribute("class", "row");
 
+// Append the container and row to the body
 document.body.append(container);
-container.appendChild(heading);
 container.appendChild(row);
 
-const displayStoreData = async ()=>{
+// Function to fetch store data from the API
+const displayStoreData = async () => {
     const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
     console.log(data);
-    return data; 
+    return data;
 }
-displayStoreData().then(data=>{
-    for(var i=0;i<data.length;i++){
-        row.innerHTML+=`
-       <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4">
-           <div class="card h-100" >
-               <div class="card-header text-center bg-dark text-white">${data[i].category}</div>
-               <img class="card-img-top" src="${data[i].image}" alt="Card image cap">
-               <div class="card-body text-center" id="card-body">
-                   <p class="card-text product-title"> ${data[i].title}</p>
-                   <p class="card-text">Rs. ${data[i].price}</p>
-                   <a href="#" class="btn btn-primary"> ADD to CART</a>
-                </div>
-           </div>
-        </div>
-       `;
-       document.body.append(container);
-   }
-   return data;
-}).then(data=>{ 
-    let cartButton=document.querySelectorAll("a");
-    for(let i=0;i<cartButton.length;i++){
-       cartButton[i].addEventListener('click',function (e){ //adding event listener for the country selected
-        e.preventDefault();
 
-            //displaying the weather data as a alert message
-            alert(`${data[i].title} Added to Cart `);
+// Function to update the cart count and cart icon
+function updateCartCount() {
+    const cartCountElement = document.querySelector(".cart-count");
+    cartCount++;
+    cartCountElement.innerText = cartCount;
+}
+
+// Function to display category-wise data
+function displayCategoryData(category) {
+    const productsContainer = document.getElementById("products-container");
+    productsContainer.innerHTML = ""; // Clear previous products
+
+    // Fetch data from the API and filter based on the selected category
+    displayStoreData().then(data => {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].category === category || category === "all") {
+                // Add product cards to the productsContainer
+                productsContainer.innerHTML += `
+                  <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+                    <div class="card h-100">
+                      <div class="card-header text-center">${data[i].category}</div>
+                      <img class="card-img-top" src="${data[i].image}" alt="Card image cap">
+                      <div class="card-body text-center" id="card-body">
+                        <p class="card-text product-title">${data[i].title}</p>
+                        <p class="card-text">Rs. ${data[i].price}</p>
+                        <a href="#" class="btn btn-primary">ADD to CART</a>
+                      </div>
+                    </div>
+                  </div>`;
+            }
+        }
+
+        // Add event listeners to the new cart buttons
+        let cartButtons = document.querySelectorAll(".btn-primary");
+        cartButtons.forEach((button, index) => {
+            button.addEventListener("click", function (e) {
+                e.preventDefault();
+                updateCartCount();
+                alert(`${data[index].title} Added to Cart`);
+            });
         });
-       }});
+    });
+}
 
+// Add event listeners to category links
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const category = link.getAttribute("data-category");
+        displayCategoryData(category);
+    });
+});
 
-
+// Initially display all products
+displayCategoryData("all");
